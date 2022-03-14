24f867654653edc1da8a6c9ce4edf75501e644f8
YogiOnBioinformatics <yraghav97@gmail.com>
2022-03-14 16:58:43 -0400

    run prepMat without normalization

--- analysis/4_prepMat/scripts/prepMat.sh
+++ analysis/4_prepMat/scripts/prepMat.sh
@@ -4,6 +4,5 @@
     /home/yraghav/CHDI-NeuroLINCS-Multi-Epigenomics-Annotation/analysis/3_define_input_bam/output/bams.input \
     -bed /home/yraghav/CHDI-NeuroLINCS-Multi-Epigenomics-Annotation/analysis/2_define_input_regions/output/all_regions.bed \
     -c 5 \
-    -norm \
     -build hg38 2>&1 \
     | tee /home/yraghav/CHDI-NeuroLINCS-Multi-Epigenomics-Annotation/analysis/4_prepMat/output/log.txt
